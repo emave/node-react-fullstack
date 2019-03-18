b76777d1aa9fd465b6c98ff5ecc7ea056e42fb8c
emave <emavericke@yahoo.com>
2019-03-19 00:27:05 +0300

    circleci settings

---
+++ client/build/precache-manifest.6beb7c83f1cc2fead84d4f6234a2ddb7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d93d660c4fad1cbbe919",
+    "url": "/static/js/runtime~main.b39a19cc.js"
+  },
+  {
+    "revision": "64778b821ebdc9ab13ff",
+    "url": "/static/js/main.969f17cd.chunk.js"
+  },
+  {
+    "revision": "0f2a17a5888d2d70bb00",
+    "url": "/static/js/6.b36ddd22.chunk.js"
+  },
+  {
+    "revision": "9a8ea941df1bba9e7116",
+    "url": "/static/js/5.82524bfa.chunk.js"
+  },
+  {
+    "revision": "2cdc74834a1ad7745bf7",
+    "url": "/static/js/4.c0660c55.chunk.js"
+  },
+  {
+    "revision": "a6536cd270a9c1429504",
+    "url": "/static/js/3.05b9df49.chunk.js"
+  },
+  {
+    "revision": "775068e7309583f33b69",
+    "url": "/static/js/0.0d7afe74.chunk.js"
+  },
+  {
+    "revision": "64778b821ebdc9ab13ff",
+    "url": "/static/css/main.b8c7e956.chunk.css"
+  },
+  {
+    "revision": "4f5514d45fb762e2edf85771a358092f",
+    "url": "/index.html"
+  }
+];
