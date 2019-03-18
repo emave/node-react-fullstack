94cbe5159de481b01dcf7d99612df797db882b99
emave <emavericke@yahoo.com>
2019-03-19 00:38:18 +0300

    circleci settings

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,6 @@
 const passport = require('passport');
 
 module.exports = app => {
-	app.get('/', () => {
-		console.log('on main page');
-	});
-
 	app.get('/auth/google', passport.authenticate('google', {
 		scope: ['profile', 'email']
 	}));
